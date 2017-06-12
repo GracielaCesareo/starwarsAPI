4ff1ad8d3a9b8fd74d5aadb189625f1d70dba7d8
Graciela Cesareo Cruz <gracielacesareocruz@Gracielas-MacBook-Pro.local>
2017-06-12 10:01:33 -0500

    AJAX

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var cargarPagina = function () {
+  cargarPersonajes();
+};
+
+var cargarPersonajes = function () {
+  $.ajax('http://swapi.co/api/people/', {
+    method: 'GET',
+    dataType: 'json',
+    success: function (response) {
+      var personajes = response.results;
+      var total = response.count;
+      mostrarTotalPersonajes(total);
+      mostrarPersonajes(personajes)
+    },
+    error: function (error) {
+        console.log("error", error);
+    }
+  });
+};
+
+var mostrarTotalPersonajes = function () {
+  $("#total").text(total);
+}
+
+var mostrarPersonaje = function (personajes) {
+  var $ul = $("#personajes");
+  personajes.forEach(function (personaje) {
+    var $li = $("<li />");
+    $li.text(personaje.name + " - " + personaje.height);
+    $ul.append($li)
+  });
+};
+
+$(document).ready(cargarPagina);
