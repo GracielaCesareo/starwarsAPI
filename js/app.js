149f27c6b00090884b73be344cb1949abe21aaf4
Graciela Cesareo Cruz <gracielacesareocruz@Gracielas-MacBook-Pro.local>
2017-06-12 11:19:24 -0500

    version con detalles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var cargarPagina = function () {
   cargarPersonajes();
+  $(document).on("click", ".personaje", mostrarDetalle);
 };
 
 var cargarPersonajes = function () {
@@ -21,9 +22,27 @@ var mostrarPersonaje = function (personajes) {
   var $ul = $("#personajes");
   personajes.forEach(function (personaje) {
     var $li = $("<li />");
+    $li.addClass("personaje");
+    $li.attr("data-url", personaje.homeworld);
     $li.text(personaje.name + " - " + personaje.height);
     $ul.append($li)
   });
 };
+
+var plantillaPlaneta = '<h2>Planeta:</h2>' +
+'<p>Nombre:<strong>__nombre__</strong></p>' +
+'<p>Clima:<strong>__clima__</strong></p>'
+
+var mostrarDetalle = function () {
+  var url = $(this).data("url");
+  var $planetaContenedor = $("#planeta");
+  $.getJSON (url, function (response) {
+    $planetaContenedor.html(
+      plantillaPlaneta.replace('__nombre__', response.name)
+        .replace('__clima__', response.climate)
+    );
+  });
+}
+
 
 $(document).ready(cargarPagina);
